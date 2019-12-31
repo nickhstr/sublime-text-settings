d789089cc1009c2b64e9a25632d93b44a8134b19
nickhstr <nickhstr@gmail.com>
2019-12-30 21:15:49 -0500

    refactor: cleanup, reorganize

--- git_blame_sublime_statusbar.py
+++ git_blame_sublime_statusbar.py
@@ -1,108 +1,158 @@
-# Copied from https://raw.githubusercontent.com/rodrigobdz/subl-gitblame-statusbar/master/git_blame_sublime_statusbar.py
+"""
+A simple Git blame plugin. Outputs to the status bar the author of, and
+time since, the current line's last edit.
+Inspiration: https://raw.githubusercontent.com/rodrigobdz/subl-gitblame-statusbar/master/git_blame_sublime_statusbar.py
+"""
 
-import sublime
-import sublime_plugin
 import os
 import subprocess
-from subprocess import check_output as shell
+from subprocess import check_output
 from datetime import datetime
 from math import floor
 import re
-
-try:
-    si = subprocess.STARTUPINFO()
-    si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-except:
-    si = None
+import sublime
+import sublime_plugin
 
 YOU = 'You'
 
+
+def parse_blame(blame):
+    """
+    Gets the username and date from Git blame output.
+
+    :param      blame:  Git blame output.
+    :type       blame:  str
+
+    :returns:   User and date last edited.
+    :rtype:     Tuple[str, str]
+    """
+    user_pattern = r'[\w ]+(?=(\d{4}-\d{2}-\d{2}))'
+    datetime_pattern = r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'
+    not_committed_pattern = 'Not Committed'
+
+    user_match = re.search(user_pattern, blame)
+    datetime_match = re.search(datetime_pattern, blame)
+
+    if user_match and datetime_match:
+        user = user_match.group(0).strip()
+
+        not_committed_match = re.search(not_committed_pattern, user)
+        if not_committed_match:
+            user = YOU
+
+        date = datetime_match.group(0).strip()
+        return (user, date)
+
+    return ('', '')
+
+
+def get_blame(line, path):
+    """
+    Gets blame information for the current line.
+
+    :param      line:  Line number in file.
+    :type       line:  str
+    :param      path:  Path to current file.
+    :type       path:  str
+
+    :returns:   Git blame output.
+    :rtype:     str
+    """
+    try:
+        return check_output(['git', 'blame', '--minimal',
+                             '-L {0},{0}'.format(line), path],
+                            cwd=os.path.dirname(os.path.realpath(path)),
+                            stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        pass
+        # print('Git blame: git error {}:\n{}'.format(e.returncode, e.output.decode('UTF-8')))
+    except Exception as e:
+        pass
+        # print('Git blame: Unexpected error:', e)
+    return ''
+
+
+def get_current_user(path):
+    """
+    Gets the current Git user.
+
+    :param      path:  Path to current file.
+    :type       path:  str
+
+    :returns:   The current user.
+    :rtype:     str
+    """
+    try:
+        return check_output(['git', 'config', 'user.name'],
+                            cwd=os.path.dirname(os.path.realpath(path)),
+                            stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        pass
+        # print('Git blame: git error {}:\n{}'.format(e.returncode, e.output.decode('UTF-8')))
+    except Exception as e:
+        pass
+        # print('Git blame: Unexpected error:', e)
+    return ''
+
+
+def time_between(date):
+    """
+    Returns the string message of how much time has elapsed since the last edit.
+
+    :param      date:  Date of edit
+    :type       date:  str
+
+    :returns:   Time since last edit.
+    :rtype:     str
+    """
+    now = datetime.now()
+    blame_date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+    delta = now - blame_date
+
+    years = round(abs(delta.days / 365))
+    months = round(abs(delta.days / 30))
+    days = abs(delta.days)
+    hours = floor(abs(delta.seconds / 60 / 60))
+    minutes = floor(abs(delta.seconds / 60))
+
+    if years > 0:
+        return "{0} {1} ago".format(years, 'years' if years > 1 else 'year')
+    if months > 0:
+        return "{0} {1} ago".format(months, 'months' if months > 1 else 'month')
+    if days > 0:
+        return "{0} {1} ago".format(days, 'days' if days > 1 else 'day')
+    if hours > 0:
+        return "{0} {1} ago".format(hours, 'hours' if hours > 1 else 'hour')
+    if minutes > 0:
+        return "{0} {1} ago".format(minutes, 'minutes' if minutes > 1 else 'minute')
+
+    return "a few seconds ago"
+
+
 class GitBlameStatusbarCommand(sublime_plugin.EventListener):
-    def parse_blame(self, blame):
-        user_pattern = '[\w ]+(?=(\d{4}-\d{2}-\d{2}))'
-        datetime_pattern = '\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'
-        not_committed_pattern = 'Not Committed'
-
-        user_match = re.search(user_pattern, blame)
-        datetime_match = re.search(datetime_pattern, blame)
-
-        if user_match and datetime_match:
-            user = user_match.group(0).strip()
-
-            not_committed_match = re.search(not_committed_pattern, user)
-            if not_committed_match:
-                user = YOU
-
-            date = datetime_match.group(0).strip()
-            return (user, date)
-
-        return ('', '')
-
-    def get_blame(self, line, path):
-        try:
-            return shell(['git', 'blame', '--minimal',
-                        '-L {0},{0}'.format(line), path],
-                        cwd=os.path.dirname(os.path.realpath(path)),
-                        startupinfo=si,
-                        stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e:
-            pass
-            # print('Git blame: git error {}:\n{}'.format(e.returncode, e.output.decode('UTF-8')))
-        except Exception as e:
-            pass
-            # print('Git blame: Unexpected error:', e)
-
-    def get_current_user(self, path):
-        try:
-            return shell(['git', 'config', 'user.name'],
-                        cwd=os.path.dirname(os.path.realpath(path)),
-                        startupinfo=si,
-                        stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e:
-            pass
-            # print('Git blame: git error {}:\n{}'.format(e.returncode, e.output.decode('UTF-8')))
-        except Exception as e:
-            pass
-            # print('Git blame: Unexpected error:', e)
-
-    def time_between(self, d):
-        now = datetime.now()
-        d = datetime.strptime(d, "%Y-%m-%d %H:%M:%S")
-        delta = now - d
-
-        years = round(abs(delta.days / 365))
-        months = round(abs(delta.days / 30))
-        days = abs(delta.days)
-        hours = floor(abs(delta.seconds / 60 / 60))
-        minutes = floor(abs(delta.seconds / 60))
-
-        if years > 0:
-            return "{0} {1} ago".format(years, 'years' if years > 1 else 'year')
-        if months > 0:
-            return "{0} {1} ago".format(months, 'months' if months > 1 else 'month')
-        elif days > 0:
-            return "{0} {1} ago".format(days, 'days' if days > 1 else 'day')
-        elif hours > 0:
-            return "{0} {1} ago".format(hours, 'hours' if hours > 1 else 'hour')
-        elif minutes > 0:
-            return "{0} {1} ago".format(minutes, 'minutes' if minutes > 1 else 'minute')
-
-        return "a few seconds ago"
+    """
+    Plugin to update status bar view with Git blame information.
+    """
 
     def on_selection_modified_async(self, view):
-        current_line = view.substr(view.line(view.sel()[0]))
-        (row, col) = view.rowcol(view.sel()[0].begin())
-        path = view.file_name()
-        curr_user = self.get_current_user(path)
-        blame = self.get_blame(int(row) + 1, path)
-        output = ''
+        """
+        Updates the status bar.
+        """
+        try:
+            row, _ = view.rowcol(view.sel()[0].begin())
+            path = view.file_name()
+            curr_user = get_current_user(path)
+            blame = get_blame(int(row) + 1, path)
+            output = ''
 
-        if blame and curr_user:
-            blame = blame.decode('utf-8')
-            curr_user = curr_user.decode('utf-8').strip()
-            user, date = self.parse_blame(blame)
-            time_since = self.time_between(date)
-            user = YOU if user == curr_user else user
-            output = user + ", " + time_since
+            if blame and curr_user:
+                blame = blame.decode('utf-8')
+                curr_user = curr_user.decode('utf-8').strip()
+                user, date = parse_blame(blame)
+                time_since = time_between(date)
+                user = YOU if user == curr_user else user
+                output = user + ", " + time_since
 
-        view.set_status('git_blame', output)
+            view.set_status('git_blame', output)
+        except:
+            pass
